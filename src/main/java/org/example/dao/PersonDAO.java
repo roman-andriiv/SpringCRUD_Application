0c341e3d40569ac7138b3ddecc19f0b865bdbe94
Roman Andriiv <roman.andriyiv@gmail.com>
2022-08-08 18:19:07 +0200

    24. CRUD application. PATCH, DELETE requests.
    add edit() method in PeopleController.java;
    create edit.html in "People" folder;
    add update() method in PeopleController.java;
    add update() method in PersonDAO.java;
    add registerHiddenFieldFilter() method in MySpringMvcDispatcherSerlvetIntitializer.java

--- src/main/java/org/example/dao/PersonDAO.java
+++ src/main/java/org/example/dao/PersonDAO.java
@@ -16,21 +16,31 @@ public class PersonDAO {
 
     {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT,"Roman"));
-        people.add(new Person(++PEOPLE_COUNT,"Alina"));
-        people.add(new Person(++PEOPLE_COUNT,"Stella"));
-        people.add(new Person(++PEOPLE_COUNT,"Ivan"));
+        people.add(new Person(++PEOPLE_COUNT, "Roman"));
+        people.add(new Person(++PEOPLE_COUNT, "Alina"));
+        people.add(new Person(++PEOPLE_COUNT, "Stella"));
+        people.add(new Person(++PEOPLE_COUNT, "Ivan"));
     }
 
-    public List<Person> index(){
+    public List<Person> index() {
         return people;
     }
-    public Person show(final int id){
-        return people.stream().filter(person ->person.getId() ==id).findAny().orElse(null);
+
+    public Person show(final int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
-    public void save(Person person){
+
+    public void save(Person person) {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
     }
 
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(person -> person.getId()==id);
+    }
 }
