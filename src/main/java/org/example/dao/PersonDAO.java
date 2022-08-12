31979845ed65649f49e39caa738f1750b68a8efa
Roman Andriiv <roman.andriyiv@gmail.com>
2022-08-12 10:28:22 +0200

    31. Automatic ID value generation  (auto increment, Serial)

--- src/main/java/org/example/dao/PersonDAO.java
+++ src/main/java/org/example/dao/PersonDAO.java
@@ -34,7 +34,7 @@ public class PersonDAO {
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO person VALUES(1, ?, ?, ?)", person.getName(),
+        jdbcTemplate.update("INSERT INTO person(name, age, email) VALUES(?, ?, ?)", person.getName(),
                 person.getAge(), person.getEmail());
     }
 
