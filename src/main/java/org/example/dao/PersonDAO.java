99a54c845bd9a0eeb610f5d2f4eb8314e0d73cbf
Roman Andriiv <roman.andriyiv@gmail.com>
2022-08-24 22:31:36 +0200

    58. Spring application with Hibernate (working app)

--- src/main/java/org/example/dao/PersonDAO.java
+++ src/main/java/org/example/dao/PersonDAO.java
@@ -1,17 +1,13 @@
 package org.example.dao;
 
 import org.example.models.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * @author Roman_Andriiv
@@ -19,10 +15,19 @@ import java.util.Optional;
 @Component
 public class PersonDAO {
 
+    private final SessionFactory sessionFactory;
 
+    @Autowired
+    public PersonDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
+    @Transactional(readOnly = true)
     public List<Person> index() {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+
+        List<Person> people = session.createQuery("select p from Person p", Person.class).getResultList();
+        return people;
     }
 
     public Person show(int id) {
--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -1,38 +1,47 @@
 package org.example.models;
 
-import javax.validation.constraints.*;
+
+//import jakarta.persistence.*;
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.Table;
+//import jakarta.persistence.Entity;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 
 /**
  * @author Roman_Andriiv
  */
+@Entity
+@Table(name = "Person")
 public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    @Column(name = "name")
     private String name;
 
     @Min(value = 0, message = "Age should be greater than 0")
+    @Column(name = "age")
     private int age;
 
-    @NotEmpty(message = "Email should not be empty")
-    @Email(message = "Email should be valid")
-    private String email;
-
-    @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{6}",
-            message = "Your address should be in the format: Country, City, Postal Code(6 digits)")
-    private String address;
-
     public Person() {
+
     }
 
-    public Person(int id, String name, int age, String email, String address) {
-        this.id = id;
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
-        this.email = email;
-        this.address = address;
     }
 
     public int getId() {
@@ -59,19 +68,12 @@ public class Person {
         this.age = age;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
