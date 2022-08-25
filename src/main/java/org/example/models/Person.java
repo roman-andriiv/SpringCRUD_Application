484eed0cde97dbcb685452ecebf9cfb61f59a86e
Roman Andriiv <roman.andriyiv@gmail.com>
2022-08-25 08:57:20 +0200

    58. Spring application with Hibernate (implement all methods)

--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -10,6 +10,7 @@ package org.example.models;
 
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
@@ -34,6 +35,11 @@ public class Person {
     @Min(value = 0, message = "Age should be greater than 0")
     @Column(name = "age")
     private int age;
+
+    @Column(name = "email")
+    @NotEmpty(message = "Email should not be empty")
+    @Email
+    private String email;
 
     public Person() {
 
@@ -66,6 +72,14 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
